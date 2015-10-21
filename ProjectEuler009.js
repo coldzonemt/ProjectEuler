e19f8d0f3d1d941d9affd0954be6caed9bb9d71d
Kristin <coldzonemt@yahoo.com>
2015-10-21 10:07:36 -0700

    9th Project Euler solution

--- ProjectEuler009.js
+++ ProjectEuler009.js
@@ -8,6 +8,22 @@ There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 */
 
-var pythagTrip = function(target) {
-  var a, b, c = 1; 
+function pythagTrip(target){
+    var i=0, k=0, j=0;
+    var arr = [];
+   for(i=0; i<target; i++){
+       for(j=0; j<target; j++){
+           for(k=0; k<target; k++){
+             //test if the sum of the three sides equals the target and makes a valid triangle
+            if(i+j+k===target && i+j > k && i+k > j && j+k > i){
+              if(i*i+j*j===k*k){
+                arr=[i,j,k]; //in case we want to know what the triplet is, I just stored it in an array
+               return i*j*k;
+              }
+            }
+          }
+       }
+   }
+  //If no Pythagorean Triplet exists that meets the target requirements, return false
+  return false;
 }
