44b0864e9c023dfb6c0d3d8a79d043b12499aa0a
Kristin <coldzonemt@yahoo.com>
2016-05-02 22:57:45 -0700

    Solution to 15th Project Euler Solution

---
+++ ProjectEuler015.js
@@ -0,0 +1,21 @@
+/*
+Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down,
+there are exactly 6 routes to the bottom right corner.
+
+How many such routes are there through a 20×20 grid?
+
+Use Pascal's Triangle / binomial coefficients to solve this. 
+*/
+
+
+var factorial = function(n) {
+	if(n === 1) {
+		return n; 
+	}
+	return n*factorial(n-1);
+};
+
+
+var paths = function(n) {
+	return (factorial(2*n))/(factorial(n)*factorial(n));
+};
